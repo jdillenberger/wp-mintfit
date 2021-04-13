b58789fb16025bd5bf44a9a33388080c9c2b5e7d
Jan Dillenberger <jdillenberger@uni-koblenz.de>
2021-04-13 22:32:56 +0200

    Add arguments to REST get-test

--- hooks/rest-get-test.php
+++ hooks/rest-get-test.php
@@ -2,22 +2,45 @@
 
 add_filter('mintfit-rest-get-test', function ($atts) {
     
-    
+    $get_trash = $_GET['trash'] ?? false;
+    $get_all_users = $_GET['all_users'] ?? false;
+
+    // Abort if a non-authorized user wants to access data for all users
+    if ($get_all_users && !current_user_can('mintfit_view_results')) {
+        return [];
+    }
+
+    // Abort if the requested test does not exist
     if (!in_array($atts['test'],  array_merge( json_decode(get_option('mintfit-api-tests', '[]'), true), ['all'] ))){
         return [];
     }
     
+    $conditions = [];
+    $condition_variables = [];
+
+    if (!$get_all_users) {
+        array_push($conditions, "`user_id` = %d");
+        array_push($condition_variables,  get_current_user_id());
+    }
+
+    if ($atts['test'] != 'all') {
+        array_push($conditions, "`test_slug` = %s");
+        array_push($condition_variables, $atts['test']);
+    }
+
+    if (!$get_trash) {
+        array_push($conditions, "`trash` IS NOT TRUE");
+    } else {
+        array_push($conditions, "`trash` IS TRUE");
+    }
+
     global $wpdb;
     $mf_table = $wpdb->prefix . 'mintfit';
+    $query = "SELECT * FROM $mf_table WHERE " . implode(' AND ', $conditions);
     
-    if($atts['test'] == 'all') {
-        $query = "SELECT * FROM $mf_table WHERE `user_id` = %d AND `trash` IS NOT TRUE";
-        $stmt = $wpdb->prepare($query, get_current_user_id());
-    } else {
-        $query = "SELECT * FROM $mf_table WHERE `user_id` = %d AND `test_slug` = %s AND `trash` IS NOT TRUE";
-        $stmt = $wpdb->prepare($query, get_current_user_id(), $atts['test']);
-    }
-    
+    $wpdb->show_errors();
+    $stmt = call_user_func_array([$wpdb, 'prepare'], array_merge([$query], $condition_variables));
+
     return array_map(function($test_result){
 
         return [
