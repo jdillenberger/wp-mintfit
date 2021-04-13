db4548ab25b061d15c19e3f5db0c405bd067d9fb
Jan Dillenberger <jdillenberger@uni-koblenz.de>
2021-04-13 22:35:23 +0200

    Remove comment

--- hooks/rest-get-options.php
+++ hooks/rest-get-options.php
@@ -2,7 +2,6 @@
 
 add_filter('mintfit-rest-get-options', function ($atts) {
 
-    //update_option('mintfit-api-tests', '[]');
     return [
         'client_id' => get_option( 'mintfit-api-client-id', '' ),
         'client_secret' => get_option( 'mintfit-api-client-secret', '' ),
