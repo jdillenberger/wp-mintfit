0a32cce6e0de43fea19fe8b77baac51ec689ffce
Jan Dillenberger <jdillenberger@uni-koblenz.de>
2021-04-16 10:38:13 +0200

    Fix naming error

--- hooks/admin-view-results.php
+++ hooks/admin-view-results.php
@@ -2,17 +2,17 @@
 
 add_action('mintfit-admin-view-results-page', function(){
 
-    wp_enqueue_script('mintfit-admin-options-js', plugin_dir_url(MF_PLUGIN_INDEX) . 'jsdist/main.js', [], false, true);
+    wp_enqueue_script('mintfit-admin-results-js', plugin_dir_url(MF_PLUGIN_INDEX) . 'jsdist/main.js', [], false, true);
     
-    wp_localize_script('mintfit-admin-options-js', 'localURLs', [
+    wp_localize_script('mintfit-admin-results-js', 'localURLs', [
         'home' => home_url(),
         'rest' => rest_url(),
         'mintfitPluginDir' => plugin_dir_url(MF_PLUGIN_INDEX),
     ]);
 
-    wp_localize_script('mintfit-admin-options-js', 'nonce', [wp_create_nonce('wp_rest')]);
+    wp_localize_script('mintfit-admin-results-js', 'nonce', [wp_create_nonce('wp_rest')]);
 
-    wp_localize_script('mintfit-admin-options-js', 'position', ['admin', 'viewResults']);
+    wp_localize_script('mintfit-admin-results-js', 'position', ['admin', 'viewResults']);
 
     ?>
     <div class="mitfit-app wrap">
